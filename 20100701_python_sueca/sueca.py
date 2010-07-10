8ea5b45c66e2d1cd4182ce21696fa33393533f2f
João Felipe N. Pimentel <joaofelipenp@gmail.com>
2010-07-10 14:05:29 -0300

    Atualizando sueca

--- 20100701_python_sueca/sueca.py
+++ 20100701_python_sueca/sueca.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 valores = {
     'A':11,
     '7':10,
@@ -19,19 +20,34 @@ class Carta(object):
         self.valor = valores[self.nome]
 
     def __gt__(self, carta):
+        if self.valor == carta.valor == 0:
+            return int(self.nome)>int(carta.nome)
         return self.valor > carta.valor
+        
 
-def somatorio(cartas):
-    return sum([carta.valor for carta in cartas])
+class Jogada(object):
+    def __init__(self, nome, carta):
+        self.nome = nome
+        self.carta = carta
+    
+    def __gt__(self, jogada):
+        return self.carta > jogada.carta
+        
+
+
+def somatorio(jogadas):
+    return sum([jogada.carta.valor for jogada in jogadas])
+
+def maior_carta(jogadas, trunfo):
+    jogadas_com_trunfo = [jogada for jogada in jogadas if jogada.carta.naipe == trunfo]
+    if jogadas_com_trunfo:
+        return max(jogadas_com_trunfo)
+    jogadas_com_carta_inicial = [jogada for jogada in jogadas if jogada.carta.naipe == jogadas[0].carta.naipe]     
+    return max(jogadas_com_carta_inicial)
+
 
 def rodada(jogadas, trunfo):
-    lista_de_cartas = [carta[1] for carta in jogadas]
-
-    pontos_vencedor = somatorio(lista_de_cartas)
-
-
-    maior_carta = max(lista_de_cartas)
-    indice_do_vencedor = lista_de_cartas.index(maior_carta)
-    vencedor = jogadas[indice_do_vencedor][0]
-
+    pontos_vencedor = somatorio(jogadas)
+    vencedor = maior_carta(jogadas,trunfo).nome
+    
     return (vencedor, pontos_vencedor)
